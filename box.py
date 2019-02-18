3711b93f6a2a4be35ea3b3482dfd4ea9d074cd71
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-18 18:59:35 -0500

    added a str function

--- box.py
+++ box.py
@@ -5,3 +5,16 @@ class Box:
         self.length = l
         self.width = w
         self.height = h
+
+    def __str__(self):
+        return "This box is {} metres long, {} metres wide, and {} metres tall.".format(self.length,self.width,self.height)
+
+print("What is the height of the box in metres?")
+height = input()
+print("What is the width of the box in metres?")
+width = input()
+print("What is the length of the box in metres?")
+length = input()
+
+my_box = Box(length,width,height)
+print(my_box)
