e52e0eca1ba5ac2a213a12762cc7c8b455fb2fd9
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-18 18:54:14 -0500

    added init function

---
+++ box.py
@@ -0,0 +1,7 @@
+#Just messing around for practice. Let's make a box!
+class Box:
+    #init function
+    def __init__(self,l=1,w=1,h=1):
+        self.length = l
+        self.width = w
+        self.height = h
