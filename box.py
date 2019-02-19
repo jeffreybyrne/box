320ce26ea8dd4c92301aa9dfcabf36ac6765b79b
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-18 19:02:42 -0500

    added a volume function

--- box.py
+++ box.py
@@ -2,12 +2,15 @@
 class Box:
     #init function
     def __init__(self,l=1,w=1,h=1):
-        self.length = l
-        self.width = w
-        self.height = h
+        self.length = int(l)
+        self.width = int(w)
+        self.height = int(h)
 
     def __str__(self):
         return "This box is {} metres long, {} metres wide, and {} metres tall.".format(self.length,self.width,self.height)
+
+    def volume(self):
+        return self.width * self.height * self.length
 
 print("What is the height of the box in metres?")
 height = input()
@@ -18,3 +21,4 @@ length = input()
 
 my_box = Box(length,width,height)
 print(my_box)
+print(my_box.volume())
