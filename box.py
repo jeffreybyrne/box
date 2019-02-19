7b05fd16b06c89c4d11b982a3b67ef009bd8bcb1
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-18 19:13:57 -0500

    added functions to increase the dimensions of the box, prompt user to increase height

--- box.py
+++ box.py
@@ -2,9 +2,9 @@
 class Box:
     #init function
     def __init__(self,l=1,w=1,h=1):
-        self.length = int(l)
-        self.width = int(w)
-        self.height = int(h)
+        self.length = float(l)
+        self.width = float(w)
+        self.height = float(h)
 
     def __str__(self):
         return "This box is {} metres long, {} metres wide, and {} metres tall.".format(self.length,self.width,self.height)
@@ -21,6 +21,23 @@ class Box:
     def width_side_area(self):
         return self.width * self.height
 
+    def increase_width(self,num):
+        self.width += num
+
+    def increase_height(self,num):
+        self.height += num
+
+    def increase_length(self,num):
+        self.length += num
+
+
+def info_box():
+    print(my_box)
+    print("The box has a volume of {} cubic metres.".format(my_box.volume()))
+    print("The box has a base area of {} squared metres.".format(my_box.base_area()))
+    print("The lengthwise face has an area of {} squared metres.".format(my_box.length_side_area()))
+    print("The widthwise face has an area of {} squared metres.".format(my_box.width_side_area()))
+
 print("What is the height of the box in metres?")
 height = input()
 print("What is the width of the box in metres?")
@@ -29,8 +46,9 @@ print("What is the length of the box in metres?")
 length = input()
 
 my_box = Box(length,width,height)
-print(my_box)
-print("The box has a volume of {} cubic metres.".format(my_box.volume()))
-print("The box has a base area of {} squared metres.".format(my_box.base_area()))
-print("The lengthwise face has an area of {} squared metres.".format(my_box.length_side_area()))
-print("The widthwise face has an area of {} squared metres.".format(my_box.width_side_area()))
+info_box()
+
+print("How much do you want to increase the height by?")
+num = float(input())
+my_box.increase_height(num)
+info_box()
