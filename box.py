9e5c23117e02d1481eb92d9d072a0715e102215a
Jeff Byrne <jeffrey.byrne@gmail.com>
2019-02-25 19:01:27 -0500

    reduced the number of methods, combined a few together

--- box.py
+++ box.py
@@ -23,48 +23,85 @@ class Box:
     def width_side_area(self):
         return self.width * self.height
 
-    #Functions to increase each dimension of the box
-    def increase_width(self):
-        print("How much do you want to increase the width by? It is currently {:.2f} metres wide.".format(self.width))
-        num = float(input())
-        self.width += num
-
-    def increase_height(self):
-        print("How much do you want to increase the height by? It is currently {:.2f} metres tall.".format(self.height))
-        num = float(input())
-        self.height += num
-
-    def increase_length(self):
-        print("How much do you want to increase the length by? It is currently {:.2f} metres long.".format(self.length))
-        num = float(input())
-        self.length += num
-
-    #Functions to decrease each dimension of the box
-    def decrease_width(self):
-        print("How much do you want to decrease the width by? It is currently {:.2f} metres wide.".format(self.width))
-        num = float(input())
-        while num >= self.width:
-            print("Width cannot be decreased by {} metres, please enter a number less than the current width of {}.".format(num,self.width))
+    def increase_dimension(self, dimension):
+        if dimension == 'length':
+            print("How much do you want to increase the length by? It is currently {:.2f} metres long.".format(self.length))
             num = float(input())
-        self.width -= num
-
-    def decrease_height(self):
-        print("How much do you want to decrease the height by? It is currently {:.2f} metres tall.".format(self.height))
-        num = float(input())
-        while num >= self.height:
-            print("Height cannot be decreased by {} metres, please enter a number less than the current height of {}.".format(num,self.height))
+            self.length += num
+        elif dimension == 'height':
+            print("How much do you want to increase the height by? It is currently {:.2f} metres tall.".format(self.height))
             num = float(input())
-        self.height -= num
-
-    def decrease_length(self):
-        print("How much do you want to decrease the length by? It is currently {:.2f} metres long.".format(self.length))
-        num = float(input())
-        while num >= self.length:
-            print("Length cannot be decreased by {} metres, please enter a number less than the current length of {}.".format(num,self.length))
+            self.height += num
+        elif dimension == 'width':
+            print("How much do you want to increase the width by? It is currently {:.2f} metres wide.".format(self.width))
             num = float(input())
-        self.length -= num
+            self.width += num
 
-    #Function to display a lot of the information about the box
+    # Functions to increase each dimension of the box
+    # def increase_width(self):
+    #     print("How much do you want to increase the width by? It is currently {:.2f} metres wide.".format(self.width))
+    #     num = float(input())
+    #     self.width += num
+    #
+    # def increase_height(self):
+    #     print("How much do you want to increase the height by? It is currently {:.2f} metres tall.".format(self.height))
+    #     num = float(input())
+    #     self.height += num
+    #
+    # def increase_length(self):
+    #     print("How much do you want to increase the length by? It is currently {:.2f} metres long.".format(self.length))
+    #     num = float(input())
+    #     self.length += num
+
+    def decrease_dimension(self, dimension):
+        if dimension == 'height':
+            print("How much do you want to decrease the height by? It is currently {:.2f} metres tall.".format(self.height))
+            num = float(input())
+            while num >= self.height:
+                print("Height cannot be decreased by {} metres, please enter a number less than the current height of {}.".format(num, self.height))
+                num = float(input())
+            self.height -= num
+        elif dimension == 'width':
+            print("How much do you want to decrease the width by? It is currently {:.2f} metres wide.".format(self.width))
+            num = float(input())
+            while num >= self.width:
+                print("Width cannot be decreased by {} metres, please enter a number less than the current width of {}.".format(num, self.width))
+                num = float(input())
+            self.width -= num
+        elif dimension == 'length':
+            print("How much do you want to decrease the length by? It is currently {:.2f} metres long.".format(self.length))
+            num = float(input())
+            while num >= self.length:
+                print("Length cannot be decreased by {} metres, please enter a number less than the current length of {}.".format(num, self.length))
+                num = float(input())
+            self.length -= num
+
+    # Functions to decrease each dimension of the box
+    # def decrease_width(self):
+    #     print("How much do you want to decrease the width by? It is currently {:.2f} metres wide.".format(self.width))
+    #     num = float(input())
+    #     while num >= self.width:
+    #         print("Width cannot be decreased by {} metres, please enter a number less than the current width of {}.".format(num,self.width))
+    #         num = float(input())
+    #     self.width -= num
+    #
+    # def decrease_height(self):
+    #     print("How much do you want to decrease the height by? It is currently {:.2f} metres tall.".format(self.height))
+    #     num = float(input())
+    #     while num >= self.height:
+    #         print("Height cannot be decreased by {} metres, please enter a number less than the current height of {}.".format(num,self.height))
+    #         num = float(input())
+    #     self.height -= num
+    #
+    # def decrease_length(self):
+    #     print("How much do you want to decrease the length by? It is currently {:.2f} metres long.".format(self.length))
+    #     num = float(input())
+    #     while num >= self.length:
+    #         print("Length cannot be decreased by {} metres, please enter a number less than the current length of {}.".format(num,self.length))
+    #         num = float(input())
+    #     self.length -= num
+
+    # Function to display a lot of the information about the box
     def box_info(self):
         print("")
         print(self)
@@ -74,7 +111,8 @@ class Box:
         print("The widthwise face has an area of {:.2f} squared metres.".format(self.width_side_area()))
         print("")
 
-#Function to get input from the user and create a new box
+
+# Function to get input from the user and create a new box
 def new_box():
     print("Welcome to the box script I made to practice making classes in Python!")
     print("What is the height of the box in metres?")
@@ -83,7 +121,7 @@ def new_box():
     width = input()
     print("What is the length of the box in metres?")
     length = input()
-    return Box(length,width,height)
+    return Box(length, width, height)
 
 #Call the function defined above and create a new box
 my_box = new_box()
@@ -97,17 +135,23 @@ while running:
     if resp == 1:
         my_box.box_info()
     elif resp == 2:
-        my_box.increase_height()
+        my_box.increase_dimension('height')
+        # my_box.increase_height()
     elif resp == 3:
-        my_box.increase_width()
+        my_box.increase_dimension('width')
+        # my_box.increase_width()
     elif resp == 4:
-        my_box.increase_length()
+        my_box.increase_dimension('length')
+        # my_box.increase_length()
     elif resp == 5:
-        my_box.decrease_height()
+        my_box.decrease_dimension('height')
+        # my_box.decrease_height()
     elif resp == 6:
-        my_box.decrease_width()
+        my_box.decrease_dimension('width')
+        # my_box.decrease_width()
     elif resp == 7:
-        my_box.decrease_length()
+        my_box.decrease_dimension('length')
+        # my_box.decrease_length()
     elif resp == 8:
         print("Thank you for using our box script today!")
         running = False
